24834d17d64ca922b53b034e2daca0a073fc33af
Marcos Dantas <marcos.dantas@g.globo>
2023-12-21 15:07:54 -0300

    first commit

---
+++ src/test/java/com/marcosdantas/crudmongodb/CrudMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosdantas.crudmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
