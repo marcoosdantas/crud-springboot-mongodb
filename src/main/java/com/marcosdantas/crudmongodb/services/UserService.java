1857b9c0544b905fd79d0aab24cec67a74c98927
Marcos Dantas <marcoosgd@gmail.com>
2023-12-27 23:31:49 -0300

    findbyid e exception

--- src/main/java/com/marcosdantas/crudmongodb/services/UserService.java
+++ src/main/java/com/marcosdantas/crudmongodb/services/UserService.java
@@ -2,18 +2,27 @@ package com.marcosdantas.crudmongodb.services;
 
 import com.marcosdantas.crudmongodb.domain.User;
 import com.marcosdantas.crudmongodb.repository.UserRepository;
+import com.marcosdantas.crudmongodb.services.exception.ObjectNotFoundException;
+import com.sun.jdi.ObjectCollectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.List;
 
 @Service
 public class UserService {
-  @Autowired
+    @Autowired
     private UserRepository repo;
 
     public List<User> findAll() {
         return repo.findAll();
 
     }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
+
